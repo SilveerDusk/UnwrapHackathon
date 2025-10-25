17801d38e7a8d04f4c94996ef5971df2f2ef8f99
Jason <Jason1@jelincic.com>
2025-10-25 08:24:43 -0700

    feat: fetches the newest 100 posts from a subreddit

--- redditCaller.py
+++ redditCaller.py
@@ -1,10 +1,13 @@
 import requests
 
 headers = {"User-Agent": "my_reddit_app:v1.0 (by u/YOUR_USERNAME)"}
-url = "https://www.reddit.com/r/uberdrivers/top.json?limit=5"
+#the limit seems to be capped at 100, so we can only get 100 posts at a time
+url = "https://www.reddit.com/r/uberdrivers/new.json?limit=10000"
 response = requests.get(url, headers=headers)
 data = response.json()
 
 for post in data["data"]["children"]:
     print(post["data"]["title"])
+
+print("Total Number of Posts Fetched: ", len(data["data"]["children"]))
 
